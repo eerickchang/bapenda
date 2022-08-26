34d82cd7d76d04d4d9d35c9443047eecaf248e6f
eerickchang <s21910250@student.unklab.ac.id>
2022-08-26 08:22:11 +0800

    ChartJs

--- client/pages/Dashboard/index.tsx
+++ client/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import {
+  BarChart,
   BoxNotification,
   DashboardHeader,
   Gap,
@@ -9,13 +10,27 @@ import {
 import styles from "./dashboard.module.css";
 import sidebarStyles from "../../components/Sidebar/sidebar.module.css";
 import Axios from "axios";
+import { UserData } from "../../components/Data";
 
 export default function Dashboard() {
-  useEffect(() => {
-    Axios.get("http://localhost:3001/masuk").then((response) => {
-      console.log(response.data.user[0]);
-    });
-  }, []);
+  // useEffect(() => {
+  //   Axios.get("http://localhost:3001/masuk").then((response) => {
+  //     console.log(response.data.user[0]);
+  //   });
+  // }, []);
+
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: UserData.map((data) => data.userGain),
+        backgroundColor: ["#1bddbb"],
+        borderRadius: 10,
+      },
+    ],
+    legend: false,
+  });
 
   return (
     <div className={styles.container}>
@@ -23,6 +38,10 @@ export default function Dashboard() {
       <Gap height={0} width={141} />
       <div className={styles.contentKiri}>
         <DashboardHeader />
+        <div className={styles.chartWrapper}>
+          <h1 className={styles.headerChart}>Grafik Kinerja Tahun 2022</h1>
+          <BarChart chartData={userData} />
+        </div>
       </div>
       <div className={styles.contentKanan}>
         <TopPegawai />
