71558e750744140b6f5ecf6cbe06490ee64a684b
eerickchang <s21910250@student.unklab.ac.id>
2022-09-30 09:22:23 +0800

    Update

--- client/pages/Kasubid/TinjauRenaksi/index.tsx
+++ client/pages/Kasubid/TinjauRenaksi/index.tsx
@@ -2,10 +2,8 @@ import React from "react";
 import { CTinjauRenaksi, Gap, Sidebar } from "../../../KasubidComponent";
 import styles from "./tinjauRenaksi.module.css";
 import sidebarStyles from "../../../KasubidComponent/Sidebar/sidebar.module.css";
-import { useRouter } from "next/router";
 
 export default function TinjauRenaksi() {
-  const router = useRouter();
   return (
     <div className={styles.container}>
       <Sidebar kotakTinjauRenaksi={sidebarStyles.kotakAktif} />
