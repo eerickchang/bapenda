830962917a7a7051219baee4072fea1299286ad8
Jeremia Waani <s21910028@student.unklab.ac.id>
2022-10-09 14:07:29 +0800

    update

--- client/pages/Kaban/CakinBidang/index.tsx
+++ client/pages/Kaban/CakinBidang/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { CProfilAdm, Gap, SidebarAdmin } from "../../../KabanComponent";
+import { CCakinBidang, Gap, SidebarAdmin } from "../../../KabanComponent";
 import styles from "./profil.module.css";
 import sidebarStyles from "../../../KabanComponent/SidebarAdmin/sidebar.module.css";
 export default function Profil() {
   return (
     <div className={styles.container}>
       <SidebarAdmin kotakProfil={sidebarStyles.kotakAktif} />
-      <Gap width={100} height={80} />
-      <CProfilAdm />
+      <Gap width={140} height={80} />
+      <CCakinBidang/>
     </div>
   );
 }
